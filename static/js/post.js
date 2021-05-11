6bc5282f67e82a5cfc4a52b34c3a661e69fafbf1
Vladimir Altov <valtov@ucsd.edu>
2021-05-10 18:32:03 -0700

    traverse site

--- static/js/post.js
+++ static/js/post.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  var post = document.getElementById("post");
+  var homepage = document.getElementById("homepage");
   var profile = document.getElementById("profile");
   var setting = document.getElementById("setting");
   var messaging = document.getElementById("messaging");
@@ -7,12 +7,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
   var messageUser = document.getElementById("messageUser");
   var home = document.getElementById("logo");
   
-  post.addEventListener("click", function() {
-    console.log("post");
+  homepage.addEventListener("click", function() {
+    console.log("homepage");
+    window.location = ('http://localhost:5000/');
   });
   
   profile.addEventListener("click", function() {
     console.log("profile");
+    window.location = ('http://localhost:5000/profile');
   });
   
   setting.addEventListener("click", function() {
--- static/js/profile.js
+++ static/js/profile.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   var post = document.getElementById("post");
+  var homepage = document.getElementById("homepage");
   var profile = document.getElementById("profile");
   var setting = document.getElementById("setting");
   var messaging = document.getElementById("messaging");
@@ -9,10 +10,16 @@ document.addEventListener("DOMContentLoaded", (event) => {
   
   post.addEventListener("click", function() {
     console.log("post");
+    window.location = ('http://localhost:5000/post');
+  });
+  homepage.addEventListener("click", function() {
+    console.log("homepage");
+    window.location = ('http://localhost:5000/');
   });
   
   profile.addEventListener("click", function() {
     console.log("profile");
+    window.location = ('http://localhost:5000/profile');
   });
   
   setting.addEventListener("click", function() {
