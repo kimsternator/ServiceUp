235a382eebbabc3295dad1baf7e89c9f6b54b05d
joshball26 <77031830+joshball26@users.noreply.github.com>
2021-05-10 18:31:23 -0700

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -1,2 +1,30 @@
 from flask import Flask
 from flask_socketio import SocketIO
+
+# flask_messenger 
+#Service up private message system built with flask
+
+#MESSAGING DATABASE 
+#**CRUD operations 
+#organize by id/sender from login 
+#deletes from one end?
+
+#MESSAGING TEMPLATE
+#*display
+#id
+#request when div is pulled into view
+#*block
+#date/timestamp
+#message 
+#sender 
+#recipient
+
+#MESSAGE DELIVERY
+#sumbition
+#sent update 
+#message status
+#notification
+
+#SECURITY CHECKS
+#json request id match senderid
+#make sure sender/receiver exists 
