cadda2ec374a126487b8d8b1a6b7bd8cc50d175a
Vladimir Altov <valtov@ucsd.edu>
2021-05-13 15:07:03 -0700

    switched to docker

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Running Instructions
+NEW DOCKER RUNNING INSTRUCTIONS
+1. [docker-compose up --build]
+2. if running first time open second terminal after this ^ command
+   and run [docker exec -it agile-web bash]
+   then inside the new bash that comes up run [python init_db.py]
+3. go to [localhost:5000]
+
+OLD VIRTUAL_ENV Running Instructions
 
 1. clone repository and cd into the directory
 2. create a virtual environment with [python3 -m venv my_venv]
--- web/src/app.py
+++ web/src/app.py
@@ -1,15 +1,32 @@
+from wsgiref.simple_server import make_server
 from flask import Flask, session, render_template, request, redirect, send_from_directory
 from google.oauth2 import id_token
 from google.auth.transport import requests
+
+import mysql.connector as mysql
 import os
 
 GOOGLE_CLIENT_ID = '675774771358-d9cs6b29kg2ce9tao1l6kq0o55s76fku.apps.googleusercontent.com'
+
+db_user = os.environ['MYSQL_USER']
+db_pass = os.environ['MYSQL_PASSWORD']
+db_name = os.environ['MYSQL_DATABASE']
+db_host = os.environ['MYSQL_HOST']
 
 # clientSecret: IQvNBGE53RwdJi3n32nABTmc
 
 app = Flask(__name__)
 app.secret_key = 'thissecretisrequired'
 
+@app.route('/database')
+def database():
+  # Connect to the database and retrieve the users
+  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+  cursor = db.cursor()
+  cursor.execute("select first_name, last_name, email from Users;")
+  records = cursor.fetchall()
+  db.close()
+  return records
 
 @app.route('/')
 def home():
@@ -58,4 +75,6 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/images'), 'favicon.png', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    server = make_server('0.0.0.0', 5000, app)
+    server.serve_forever()
