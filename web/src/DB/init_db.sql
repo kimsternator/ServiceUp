f690de07e285babb744f864b262114efa6884c00
joshball26 <77031830+joshball26@users.noreply.github.com>
2021-05-21 14:37:07 -0700

    Delete init_db.sql

--- web/src/DB/init_db.sql
+++
@@ -1,9 +0,0 @@
-init_db
-
-CREATE TABLE IF NOT EXISTS messages (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  dt TEXT NOT NULL,
-  message TEXT NOT NULL,
-  receiver TEXT NOT NULL,
-  sender TEXT NOT NULL
-);
